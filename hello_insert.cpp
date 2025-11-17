427082f940241526dc970e320d5c4488ef6d2250
tennehkanneh <tennehkanneh@u.boisestate.edu>
2025-11-16 23:18:57 -0700

    Completed Hello Inster program with comments.

--- hello_insert.cpp
+++ hello_insert.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-using namespace std;
+using namespace std;    // standard libray (std, string, cout, cin, etc.)
 
 int main() 
 {
     std:string name;
+    
+    cout << "What is your name?\n";
+    cin >> name;    // Taking input from user and store it in variable
+
+    // Output "Hello [name]"
+    cout << "Hello " << name << "!";
 
     return 0;
 }
